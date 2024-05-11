5017474a7de23c4e604a387c4e10a7aa9618f31b
Fensley <zagalufe@gmail.com>
2024-05-11 01:30:15 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+// import App from "./App";
+import StarRating from "./StartRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating maxRating={10} color="blue" onSetRating={setMovieRating} />
+      <p> This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <StarRating
+      maxRating={5}
+      messages={["terrible", "bad", "okay", "Good", "Amazing"]}
+    />
+    <StarRating size={20} color="green" className="test" defaultRating={2} />
+
+    <Test />
+    {/* <App /> */}
   </React.StrictMode>
 );

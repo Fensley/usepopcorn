195f5bb40afb5357c177b6198e3c92e1de967d83
Fensley <zagalufe@gmail.com>
2024-06-10 21:28:07 -0400

    initial commit

---
+++ src/useLocalStorageState.js
@@ -0,0 +1,15 @@
+import { useState , useEffect } from "react";
+export function useLocalStorageState(initialState, key){
+     const [value, setvalue] = useState(function (){
+  const storedValue = localStorage.getItem("watched");
+  return storedValue ? JSON.parse(storedValue) : initialState
+ });
+
+   useEffect(
+    function (){
+      localStorage.setItem("watched" , JSON.stringify(value))
+    } , [value, key]
+  )
+
+  return [value , setvalue]
+}
